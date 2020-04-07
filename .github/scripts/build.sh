f17823e0d2539a6a815097a76d6abe3c2f5b4fc0
Shengjing Zhu <i@zhsj.me>
2020-04-07 17:13:14 +0800

    Fix broken CI caused by Sphinx 3.0
    Closes: #107

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -13,5 +13,11 @@ error() {
 cd cpython/Doc || exit 1
 mkdir -p locales/"$LOCALE"/
 ln -sfn "$(realpath ../../docs)" locales/"$LOCALE"/LC_MESSAGES
-make venv
-make html SPHINXOPTS="-D language=$LOCALE -D gettext_compact=0 -W --keep-going -j2" 2> >(error)
+
+# make venv
+# create venv manually, so we can specify sphinx version
+VENVDIR=./venv
+python3 -m venv $VENVDIR
+$VENVDIR/bin/python3 -m pip install -U pip setuptools
+$VENVDIR/bin/python3 -m pip install -U "Sphinx<3.0" blurb python-docs-theme
+make html VENVDIR=$VENVDIR SPHINXOPTS="-D language=$LOCALE -D gettext_compact=0 -W --keep-going -j2" 2> >(error)
