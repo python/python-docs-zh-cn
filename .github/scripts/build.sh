065e4c9d87833baa6065ea7f905c6ecc1876c4d4
Shengjing Zhu <i@zhsj.me>
2021-11-03 00:27:36 +0800

    Pin docutils version when build with Sphinx 2.3.1
    Closes: #227

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -13,5 +13,8 @@ error() {
 cd cpython/Doc || exit 1
 mkdir -p locales/"$LOCALE"/
 ln -sfn "$(realpath ../../docs)" locales/"$LOCALE"/LC_MESSAGES
+
+# Hotfix for #227
+sed -i 's|Sphinx==2.3.1|Sphinx==2.3.1 "docutils<0.18"|g'  Makefile
 make venv
 make html SPHINXOPTS="-D language=$LOCALE -D gettext_compact=0 -W --keep-going -j2" 2> >(error)
