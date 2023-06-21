296c3d3eb94f299e25046ea15cb1c7f37690b4a7
A <793763955@qq.com>
2023-06-21 11:58:13 +0800

    search

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -14,4 +14,4 @@ cd cpython/Doc || exit 1
 mkdir -p locales/"$LOCALE"/
 ln -sfn "$(realpath ../../docs)" locales/"$LOCALE"/LC_MESSAGES
 pip3 install -q -r requirements.txt
-make html SPHINXOPTS="-D language=$LOCALE -D gettext_compact=0 -D html_copy_source=False -W --keep-going" 2> >(error)
+make html SPHINXOPTS="-D language=$LOCALE -D gettext_compact=0 -D html_search_language='' -W --keep-going" 2> >(error)
