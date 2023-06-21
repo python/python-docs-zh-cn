3040c4b8056c6dd46cdbfdc56c3d7205f7a6c200
A <793763955@qq.com>
2023-06-21 10:56:04 +0800

    remove venv

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -2,7 +2,4 @@
 
 set -ex
 
-pip3 install --user setuptools
 curl -o- https://raw.githubusercontent.com/transifex/cli/master/install.sh | bash
-sudo apt-get update
-sudo apt-get install -y python3-venv
