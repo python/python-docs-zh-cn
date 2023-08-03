8530ab102ab2b282c5f079903e2526bba106887f
A <5249513+Dumeng@users.noreply.github.com>
2023-08-04 00:48:01 +0800

    Add tx stat

---
+++ .github/scripts/tx_stat.py
@@ -0,0 +1,31 @@
+import json
+import os
+import urllib.request
+
+key = os.environ.get('TX_TOKEN')
+project = os.environ.get('TX_PROJECT')
+
+url = "https://rest.api.transifex.com/resource_language_stats?filter[project]=o%3Apython-doc%3Ap%3A{}&filter[language]=l%3Azh_CN".format(project)
+
+headers = {
+    "accept": "application/vnd.api+json",
+    "authorization": "Bearer " + key
+}
+
+total = 0
+translated = 0
+
+while(url):
+    request = urllib.request.Request(url=url,headers=headers)
+
+    with urllib.request.urlopen(request) as response:
+        data = json.loads(response.read().decode("utf-8"))
+        url = data['links'].get('next')
+        for resourse in data['data']:
+            translated = translated + resourse['attributes']['translated_strings']
+            total =  total + resourse['attributes']['total_strings']
+
+p = '{:.2%}'.format(translated/total)
+
+with open(".stat.json", "w") as text_file:
+    text_file.write(json.dumps({'translation':p}))
