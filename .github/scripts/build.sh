0aa564649b0b67235e0092f24cc36d7046360c37
A <5249513+Dumeng@users.noreply.github.com>
2023-08-03 22:30:14 +0800

    remvoe docsbuild-scripts

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -13,9 +13,4 @@ error() {
 cd cpython/Doc || exit 1
 mkdir -p locales/"$LOCALE"/
 ln -sfn "$(realpath ../../docs)" locales/"$LOCALE"/LC_MESSAGES
-pip3 install -q -r requirements.txt
-
-cd ../../docsbuild/
-pip3 install -q -r requirements.txt
-python3 ./build_docs.py --quick --build-root ./build_root --www-root ./www --skip-cache-invalidation --language $LOCALE --branch $VERSION
-# make html SPHINXOPTS="-D language=$LOCALE -D gettext_compact=0 -W --keep-going" 2> >(error)
+make html SPHINXOPTS="-D language=$LOCALE -D gettext_compact=0 -W --keep-going" 2> >(error)
