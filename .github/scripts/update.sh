2896f1d65ec71ee1d28642b558d22aa0f7e3ac30
A <5249513+Dumeng@users.noreply.github.com>
2023-08-04 16:05:57 +0800

    Restore git timestamp with another method

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -3,19 +3,9 @@
 cd docs || exit 1
 
 # Restore git timestamp for enabling build cache
-if [ $# = 0 ]; then
-  git ls-files -z |xargs -0 sh "$0"
-  exit $?
-fi
-
-for file in "$@"; do
-  time="$(git log --pretty=format:%cd -n 1 \
-                  --date=format:%Y%m%d%H%M.%S --date-order -- "$file")"
-  if [ -z "$time" ]; then
-    echo "ERROR: skipping '$file' -- no git log found" >&2
-    continue
-  fi
-  touch -m -t "$time" "$file"
+rev=HEAD
+for f in $(git ls-tree -r -t --full-name --name-only "$rev") ; do
+    touch -d $(git log --pretty=format:%cI -1 "$rev" -- "$f") "$f";
 done
 
 $(realpath ../tx) pull --languages "$LOCALE" -t --use-git-timestamps --workers 25 --silent
