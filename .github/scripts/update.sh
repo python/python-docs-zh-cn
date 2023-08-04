7e2b982ea576e229f0ced6016f74b67869be021f
A <5249513+Dumeng@users.noreply.github.com>
2023-08-04 16:17:33 +0800

    Restore git timestamp

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+cd cpython || exit 1
+
+# Restore git timestamp for enabling build cache
+rev=HEAD
+for f in $(git ls-tree -r -t --full-name --name-only "$rev") ; do
+    touch -d $(git log --pretty=format:%cI -1 "$rev" -- "$f") "$f";
+done
+
+cd ..
 cd docs || exit 1
 
 # Restore git timestamp for enabling build cache
