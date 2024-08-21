c6514dd2d9f205de8509d4c78c4020ec7d73b2e6
A <5249513+Dumeng@users.noreply.github.com>
2024-08-21 20:34:56 +0800

    update build command to use fresh environment
    Attempt to resolve the environment cache cause building stuck issues.

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -15,4 +15,4 @@ cd cpython/Doc || exit 1
 mkdir -p locales/"$LOCALE"/
 ln -sfn "$(realpath ../../docs)" locales/"$LOCALE"/LC_MESSAGES
 pip3 install -q -r requirements.txt
-sphinx-build -b dummy -d build/doctrees  -j auto -D language=$LOCALE -D gettext_compact=0 -W --keep-going -W . build/html 2> >(error)
+sphinx-build -b dummy -d build/doctrees  -j auto -D language=$LOCALE -D gettext_compact=0 -E --keep-going -W . build/html 2> >(error)
