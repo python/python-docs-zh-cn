8e0a0fd29f68b83c8e1d24660a329a382102362b
A <5249513+Dumeng@users.noreply.github.com>
2024-10-11 19:20:11 +0800

    Fix error: externally-managed-environment in GitHub Actions runner

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -14,5 +14,5 @@ error() {
 cd cpython/Doc || exit 1
 mkdir -p locales/"$LOCALE"/
 ln -sfn "$(realpath ../../docs)" locales/"$LOCALE"/LC_MESSAGES
-pip3 install -q -r requirements.txt
+pip3 install -q -r requirements.txt --break-system-packages
 sphinx-build -b dummy -d build/doctrees  -j auto -D language=$LOCALE -D gettext_compact=0 -E --keep-going -W . build/html 2> >(error)
